0b86e4c000a3e5d0c736c1519dff328fee422c77
David Busch <busch@users.noreply.github.com>
2019-08-28 13:03:33 -0500

    Function demo

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+// function prototype
+void Output();
+
+int main()
+{
+	// function call
+	Output();
+
+	return 0;
+}
+
+// function defintion
+void Output()
+{
+	cout << "Hello" << endl;
+}
