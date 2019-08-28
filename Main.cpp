b57cd739c0c716d114c3ac1628f877da68bd3964
David Busch <busch@users.noreply.github.com>
2019-08-28 13:43:20 -0500

    Add variable declaration

--- Main.cpp
+++ Main.cpp
@@ -2,19 +2,25 @@
 
 using namespace std;
 
+const float pi = float(3.14159265358979323846264338);
+
 // function prototype
-void Output();
+void Output(int value);
 
 int main()
 {
+	// variable declaration
+	int number = 5;
+
 	// function call
-	Output();
+	Output(number);
 
 	return 0;
 }
 
 // function defintion
-void Output()
+void Output(int value)
 {
-	cout << "Hello" << endl;
+	cout << "Hello " << value << " " << pi;
+	cout << endl;
 }
