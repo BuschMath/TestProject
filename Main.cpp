2a2cf27d1617b4537d72f34cd9b8c669598ffe49
David Busch <busch@users.noreply.github.com>
2019-08-29 14:31:44 -0500

    Pass Data to Function and Output

--- Main.cpp
+++ Main.cpp
@@ -5,22 +5,34 @@ using namespace std;
 const float pi = float(3.14159265358979323846264338);
 
 // function prototype
-void Output(int value);
+void Output(string, string, string, string, const string);
+
+int Add(int, int);
 
 int main()
 {
 	// variable declaration
-	int number = 5;
+	string name = "Jane Doe";
+	string phoneNumber = "5555551234";
+	string email = "doe@gmail.com";
+	string ccNumber = "12345678910";
+	const string birthday = "23091973";		// code is day month year
 
 	// function call
-	Output(number);
+	Output(name, phoneNumber, email, ccNumber, birthday);
+
+	cout << Add(3, 4);
 
 	return 0;
 }
 
 // function defintion
-void Output(int value)
+void Output(string N, string P, string E, string C, const string B)
 {
-	cout << "Hello " << value << " " << pi;
-	cout << endl;
+	cout << N << "\t" << P << endl << E << "\t" << C << endl << B << endl;
+}
+
+int Add(int input1, int input2)
+{
+	return input1 + input2;
 }
