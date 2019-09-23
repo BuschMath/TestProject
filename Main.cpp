a37e8b689a014d8801311c140641d753e69b30fc
David Busch <busch@users.noreply.github.com>
2019-09-23 13:36:24 -0500

    Exam No: 8

--- Main.cpp
+++ Main.cpp
@@ -1,32 +1,20 @@
 #include <iostream>
-#include <cmath>
-#include <iomanip>
 
 using namespace std;
 
-const double pi = 3.14159265358979323846264338;
-
-double volumeOfCy(double radius, double height);
-double cir(double radius);
-
 int main()
 {
-	double radius = 15;
-	double height = 0.5;
+	string letters = "ABCDE";
 
-	cout << "The volume is:" << setw(13) << volumeOfCy(radius, height) + 1000000 << endl;
-	cout << cir(radius) << endl;
-	cout << fixed << setprecision(6) << float(100) << endl;
+	cout << letters;
+
+	cout << "\n\n\nThe following is a calculated result: ";
+
+	float x = 3.1415;
+	double y = 1.2345;
+	char z = 'T';
+
+	cout << char((2 * x - pow(y, 3)) / 4 * z);
 
 	return 0;
-}
-
-double volumeOfCy(double radius, double height)
-{
-	return pi * pow(radius, 2) * height;
-}
-
-double cir(double radius)
-{
-	return 2 * pi * radius;
 }
